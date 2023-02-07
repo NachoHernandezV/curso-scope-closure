2f0471b9590d9041672ba93eb15998bae0df8ac6
NachoHernandezV <hjoseignacio@hotmail.com>
2023-02-07 11:22:20 -0600

    reasignar

---
+++ src/scope/reasignar.js
@@ -0,0 +1,11 @@
+let one = 1;  //asignar y declarar
+one = 2; //asignar
+console.log(one)
+
+var two = 1;  //asignar y declarar
+var two = 2; //se puede reasignar
+console.log(two)
+
+const three = 3;  //asignar y declarar
+//const three = 2; // no se puede reasignar
+console.log(three)
