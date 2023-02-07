8a89bd2ee0e6fb95897de5c49460662e94dffc12
NachoHernandezV <hjoseignacio@hotmail.com>
2023-02-07 10:49:33 -0600

    add diferences var let const

---
+++ src/scope/block.js
@@ -0,0 +1,14 @@
+function fruits(){
+
+    if (true){
+        var banana = "SOY FRUTA BANANA";
+        let pineapple =" SOY UNA PIÑA";
+        const apple ="soy una manzana";
+    }
+
+    console.log(banana);
+   // console.log(pineapple);
+    //console.log(apple);
+}
+
+fruits();
